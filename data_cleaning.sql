2b37dea83ff908d588dd4f6d4232a70a64e0acc0
codeunmasked <codeunmasked@gmail.com>
2026-02-25 14:08:46 +0530

    Create call_center_clean table from call_center_raw

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE call_center_clean AS
+SELECT DISTINCT
+    call_id,
+    agent_name,
+    customer_name,
+    call_date,
+    call_time,
+    COALESCE(call_duration, 0) AS call_duration,
+    COALESCE(issue_type, 'Unknown') AS issue_type,
+    COALESCE(call_status, 'Unresolved') AS call_status
+FROM call_center_raw;
