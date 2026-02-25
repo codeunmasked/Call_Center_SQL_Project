3fc605d3241d855b9c07c58165c7fed34757d300
codeunmasked <codeunmasked@gmail.com>
2026-02-25 14:08:01 +0530

    Create call_center_raw table in SQL

--- create_table.sql
+++ create_table.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE call_center_raw (
+    call_id INT,
+    agent_name VARCHAR(50),
+    customer_name VARCHAR(50),
+    call_date DATE,
+    call_time TIME,
+    call_duration INT,
+    issue_type VARCHAR(50),
+    call_status VARCHAR(20)
+);
